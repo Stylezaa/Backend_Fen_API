95ae63492a8e29ead9d14c57a8e5b79f09f0525f
Two Archineer <archineertwo@Twos-Mac-mini.local>
2022-06-21 13:37:18 +0700

    Close Request Limit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 // //Protect stack for build
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit')
+// const rateLimit = require('express-rate-limit')
 
 //import middleware
 const errorHandler = require("./middleware/errorHandler");
@@ -34,15 +34,15 @@ app.use(cors());
 app.use(helmet());
 
 //Set Rate limit for Required Api
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+// const limiter = rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// })
 
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+// app.use(limiter)
 
 var whitelist = ['http://localhost:3000']
 var corsOptions = {
